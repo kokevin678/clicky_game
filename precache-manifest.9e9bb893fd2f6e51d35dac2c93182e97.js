82787527a928d6619784199759158dde789e9f3a
Chengyu Ko <kokevin678@gmail.com>
2020-02-07 22:20:21 -0800

    Updates

---
+++ precache-manifest.9e9bb893fd2f6e51d35dac2c93182e97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68430673aebc3065daf648246ae30e0e",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "81f8ff2a54364565e9df",
+    "url": "/clicky_game/static/css/main.bdc2ebe9.chunk.css"
+  },
+  {
+    "revision": "53f498e5c9e2fe769c46",
+    "url": "/clicky_game/static/js/2.e35cc67b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky_game/static/js/2.e35cc67b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f8ff2a54364565e9df",
+    "url": "/clicky_game/static/js/main.928be047.chunk.js"
+  },
+  {
+    "revision": "98a991019910cbc1107f",
+    "url": "/clicky_game/static/js/runtime-main.efcabb2a.js"
+  }
+]);
